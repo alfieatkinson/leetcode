1eb4ad825cba2f822c4cbf85803b1a5755336489
Alfie Atkinson <aatkinson@b-hiveinnovations.co.uk>
2024-06-21 15:26:55 +0100

    2423 First try

--- 2423_remove-letter-to-equalize-frequency/Solution.py
+++ 2423_remove-letter-to-equalize-frequency/Solution.py
@@ -1,3 +1,25 @@
 class Solution:
     def equalFrequency(self, word: str) -> bool:
-        pass
+        if len(word) > 26:
+            return False
+        
+        char_freq = {char: word.count(char) for char in word}
+        #print(char_freq)
+
+        freq_freq = {freq: list(char_freq.values()).count(freq) for freq in char_freq.values()}
+        #print(freq_freq)
+
+        if len(freq_freq) == 1:
+            return True
+        if len(freq_freq) > 2:
+            return False
+        if 1 in freq_freq.values():
+            return True
+        return False
+    
+tests = ["abcc", "aazz", "abccdd", "abc", "abbcc"]
+
+S = Solution()
+
+for test in tests:
+    print(S.equalFrequency(test))
