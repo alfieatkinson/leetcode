a24e5374531b4a211c248be2cc38c2a1e0982883
Alfie Atkinson <aatkinson@b-hiveinnovations.co.uk>
2024-06-21 14:48:06 +0100

    8 Second try

--- 8_string-to-integer-(atoi)/Solution.py
+++ 8_string-to-integer-(atoi)/Solution.py
@@ -19,11 +19,13 @@ class Solution:
         if len(n) == 0 or n == '-':
             n = "0"
 
-        return int(n)
+        if int(n) < -2**31: return -2**31
+        elif int(n) > 2**31 - 1: return 2**31 - 1
+        else: return int(n)
             
 S = Solution()
 
-tests = ["42", "   -42", "1337c0d3", "0-1", "words and 987"]
+tests = ["42", "   -42", "1337c0d3", "0-1", "words and 987", "-91283472332"]
 
 for s in tests:
     print(S.myAtoi(s))
