ce4780d8fed0f3e65969c48343443591322f1ab9
Alfie Atkinson <aatkinson@b-hiveinnovations.co.uk>
2024-06-21 13:26:51 +0100

    1052 Third try

--- 1052_grumpy-bookstore-owner/Solution.py
+++ 1052_grumpy-bookstore-owner/Solution.py
@@ -1,37 +1,4 @@
-from functools import reduce
-
 class Solution:
-    def window_check(self, customers: list[int], grumpy: list[int], minutes: int, start: int) -> int:
-        """
-        @param customers (list[int]): the amount of customers each minute.
-        @param grumpy (list[int]): whether the shopkeeper is grumpy or not each minute.
-        @param minutes (int): the number of minutes the shopkeeper's ability lasts.
-        @param start (int): the start of the ability window.
-        @return satisfied (int): the amount of satisfied customers with the ability activated in this window.
-        """
-        n = len(customers)
-
-        grumpy_ability = [i for i in grumpy]
-
-        pointers = ['^' if i == start or i == start + minutes - 1 else ' ' for i in range(0, n)]
-
-        for i in range(start, start + minutes):
-            grumpy_ability[i] = 1
-
-        satisfied_customers = [customers[i] * grumpy_ability[i] for i in range(0, n)]
-
-        print(f"""
-Customers:           {customers}
-Grumpy:              {[1 if i == 0 else 0 for i in grumpy]}
-Grumpy with window:  {[1 if i == 0 else 0 for i in grumpy_ability]}
-                     {f" {'  '.join(pointers)} "}
-Satisfied customers: {satisfied_customers}
-""")
-
-        satisfied = sum(satisfied_customers)
-
-        return satisfied
-
     def maxSatisfied(self, customers: list[int], grumpy: list[int], minutes: int) -> int:
         """
         @param customers (list[int]): the amount of customers each minute.
@@ -42,22 +9,38 @@ Satisfied customers: {satisfied_customers}
         n = len(customers)
 
         # Invert grumpy list so that grumpy is 0 to cancel out customers on grumpy days
-        inverted_grumpy = [1 if i == 0 else 0 for i in grumpy] 
+        grumpy = [1 if i == 0 else 0 for i in grumpy]
 
         # Cancel out customers on grumpy days and get sum of customers on non grumpy days
-        satisfied = sum([customers[i] * inverted_grumpy[i] for i in range(0, n)]) 
+        satisfied = sum([customers[i] * grumpy[i] for i in range(0, n)]) 
+        max_sat = satisfied
 
-        window = 0
-        for i in range(0, n - minutes + 1):
-            window_sat = self.window_check(customers, inverted_grumpy, minutes, i)
-            if window_sat > satisfied: satisfied = window_sat
+        print(f"Satisfied customers with no ability used: {satisfied}.")
 
-        return satisfied
+        for i in range(0, minutes):
+            if grumpy[i] == 0:
+                satisfied += customers[i]
+        
+        if satisfied > max_sat: max_sat = satisfied
+
+        print(f"Satisfied customers with ability used in minute 1: {satisfied}.")
+
+        for i in range(1, n - minutes + 1):
+            print(f"Customers: {customers[i:i+minutes]}")
+            if grumpy[i - 1] == 0:
+                satisfied -= customers[i - 1]
+            if grumpy[i + minutes - 1] == 0:
+                satisfied += customers[i + minutes - 1]
+
+            if satisfied > max_sat: max_sat = satisfied
+            print(f"Satisfied customers with ability used in minute {i + 1}: {satisfied}.")
+
+        return max_sat
     
 S = Solution()
 
 customers = [1,0,1,2,1,1,7,5]
-grumpy = [0,1,0,1,0,1,0,1]
+grumpy =    [0,1,0,1,0,1,0,1]
 minutes = 3
 
 print(S.maxSatisfied(customers, grumpy, minutes))
