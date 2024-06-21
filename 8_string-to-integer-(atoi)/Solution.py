c9534e074f809e31885bcb1be6e40c9e3a6d5807
Alfie Atkinson <aatkinson@b-hiveinnovations.co.uk>
2024-06-21 14:44:09 +0100

    8 First try

--- 8_string-to-integer-(atoi)/Solution.py
+++ 8_string-to-integer-(atoi)/Solution.py
@@ -1,3 +1,29 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        pass
+        s = s.strip()
+        n = ''
+
+        if s[0] == '-':
+            n += '-'
+            s = s[1:]
+        elif s[0] == '+':
+            s = s[1:]
+
+        for i in s:
+            if i.isnumeric():
+                if int(i) > 0:
+                    n += i
+            else:
+                break
+        
+        if len(n) == 0 or n == '-':
+            n = "0"
+
+        return int(n)
+            
+S = Solution()
+
+tests = ["42", "   -42", "1337c0d3", "0-1", "words and 987"]
+
+for s in tests:
+    print(S.myAtoi(s))
