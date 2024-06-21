3bc39bbd05436110f7fe6d36def96185789d6765
Alfie Atkinson <aatkinson@b-hiveinnovations.co.uk>
2024-06-21 12:17:14 +0100

    1052 Second try

--- 1052_grumpy-bookstore-owner/Solution.py
+++ 1052_grumpy-bookstore-owner/Solution.py
@@ -11,10 +11,24 @@ class Solution:
         """
         n = len(customers)
 
-        for i in range(start, start + minutes + 1):
-            grumpy[i] = 1
+        grumpy_ability = [i for i in grumpy]
 
-        satisfied = sum([customers[i] * grumpy[i] for i in range(0, n)])
+        pointers = ['^' if i == start or i == start + minutes - 1 else ' ' for i in range(0, n)]
+
+        for i in range(start, start + minutes):
+            grumpy_ability[i] = 1
+
+        satisfied_customers = [customers[i] * grumpy_ability[i] for i in range(0, n)]
+
+        print(f"""
+Customers:           {customers}
+Grumpy:              {[1 if i == 0 else 0 for i in grumpy]}
+Grumpy with window:  {[1 if i == 0 else 0 for i in grumpy_ability]}
+                     {f" {'  '.join(pointers)} "}
+Satisfied customers: {satisfied_customers}
+""")
+
+        satisfied = sum(satisfied_customers)
 
         return satisfied
 
@@ -34,7 +48,7 @@ class Solution:
         satisfied = sum([customers[i] * inverted_grumpy[i] for i in range(0, n)]) 
 
         window = 0
-        for i in range(0, n - minutes):
+        for i in range(0, n - minutes + 1):
             window_sat = self.window_check(customers, inverted_grumpy, minutes, i)
             if window_sat > satisfied: satisfied = window_sat
 
@@ -42,8 +56,8 @@ class Solution:
     
 S = Solution()
 
-customers = [10, 1, 7]
-grumpy = [0, 0, 0]
-minutes = 2
+customers = [1,0,1,2,1,1,7,5]
+grumpy = [0,1,0,1,0,1,0,1]
+minutes = 3
 
 print(S.maxSatisfied(customers, grumpy, minutes))
