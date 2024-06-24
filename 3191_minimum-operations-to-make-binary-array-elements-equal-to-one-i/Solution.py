1a42787b3dc4d26d6ad6d7213b6218f97378087a
Alfie Atkinson <aatkinson@b-hiveinnovations.co.uk>
2024-06-24 13:39:43 +0100

    3191 First try

--- 3191_minimum-operations-to-make-binary-array-elements-equal-to-one-i/Solution.py
+++ 3191_minimum-operations-to-make-binary-array-elements-equal-to-one-i/Solution.py
@@ -1,3 +1,27 @@
 class Solution:
-    def minOperations(self, nums: List[int]) -> int:
-        pass
+    def minOperations(self, nums: list[int]) -> int:
+        """
+        @param nums (list[int]): a list of 0s and 1s.
+        @return flips (int): the minimum number of flips required to make all 1s.
+        """
+        k = 3
+        n = len(nums)
+        flips = 0
+        flipped = [0] * n
+        flip_count = 0
+
+        for i in range(n):
+            if i >= k:
+                flip_count -= flipped[i - k]
+            
+            if (nums[i] + flip_count) % 2 == 0:
+                if i + k > n:
+                    print(f"Flips: {flips}\nFlipped: {flipped}\nFlip Count: {flip_count}\n")
+                    return -1
+                flips += 1
+                flip_count += 1
+                flipped[i] = 1
+
+            print(f"Flips: {flips}\nFlipped: {flipped}\nFlip Count: {flip_count}\n")
+        
+        return flips
