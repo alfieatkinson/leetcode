b58d0f76c4918cc2ee6a353bc2dd9f65d5ce536d
Alfie Atkinson <alf.atkinson13@gmail.com>
2024-06-28 20:01:03 +0100

    2285 First try

--- 2285_maximum-total-importance-of-roads/Solution.py
+++ 2285_maximum-total-importance-of-roads/Solution.py
@@ -1,22 +1,33 @@
 class Solution:
     def maximumImportance(self, n: int, roads: list[list[int]]) -> int:
-        connections = [[] for i in range(n)]
+        degrees = [0] * n
 
         for road in roads:
-            connections[road[0]].append(road[1])
-            connections[road[1]].append(road[0])
+            degrees[road[0]] += 1
+            degrees[road[1]] += 1
 
-        print(connections)
+        print(degrees)
 
-        connections_sorted = sorted(connections, key=len, reverse=True)
+        sorted_cities = sorted(range(n), key=lambda x: degrees[x], reverse=True)
 
-        print(connections_sorted)
+        print(sorted_cities)
 
-        importances = [n - connections_sorted.index(i) for i in connections]
+        importances = [0] * n
+        current_importance = n
+
+        for city in sorted_cities:
+            importances[city] = current_importance
+            current_importance -= 1
 
         print(importances)
 
-roads = [[0,1],[1,2],[2,3],[0,2],[1,3],[2,4]]
+        total = 0
+        for road in roads:
+            total += importances[road[0]] + importances[road[1]]
+
+        return total
+
+roads = [[0,3],[2,4],[1,3]]
 
 S = Solution()
 
