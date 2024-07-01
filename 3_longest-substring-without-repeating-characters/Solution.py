01b47c99da521cac36c9ee3effef41d10725859c
Alfie Atkinson <aatkinson@b-hiveinnovations.co.uk>
2024-07-01 14:09:05 +0100

    3 Second try

--- 3_longest-substring-without-repeating-characters/Solution.py
+++ 3_longest-substring-without-repeating-characters/Solution.py
@@ -1,3 +1,25 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        pass
+        substring = ""
+
+        start = 0
+        for i, char in enumerate(s):
+            window = s[start:i+1]
+            print(i, char)
+            print(window)
+            if char not in window[:-1]:
+                if len(window) > len(substring):
+                    substring = window
+            else:
+                while char in window[:-1]:
+                    start += 1
+                    window = s[start:i+1]
+                    print(window)
+        print(substring)
+        return len(substring)
+
+s = "abcabcbb"
+
+solution = Solution()
+
+print(solution.lengthOfLongestSubstring(s))
