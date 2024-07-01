1963eab17a39676edc17ed7cd8e8ae2aefe0ed0f
Alfie Atkinson <aatkinson@b-hiveinnovations.co.uk>
2024-07-01 08:42:14 +0100

    1550 First try

--- 1550_three-consecutive-odds/Solution.py
+++ 1550_three-consecutive-odds/Solution.py
@@ -1,3 +1,21 @@
 class Solution:
     def threeConsecutiveOdds(self, arr: list[int]) -> bool:
-        pass
+        n = len(arr)
+        odds = list(map(lambda x: x % 2, arr))
+
+        for i in range(n - 2):
+            print(f"""
+N: {i}
+Arr: {arr[i]}
+Odds: {odds[i]}
+""")
+            if odds[i] and odds[i + 1] and odds[i + 2]:
+                print(arr[i:i + 3])
+                return True
+        return False
+    
+arr = [1,2,34,3,4,5,7,23,12]
+
+S = Solution()
+
+print(S.threeConsecutiveOdds(arr))
